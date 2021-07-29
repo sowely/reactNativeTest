a2e2abd7e5f6c587b9ac4a238df77ecaecf31c0b
sowely <pikulinadasha99@gmail.com>
2021-07-29 16:22:25 +0700

    testApp with useState & useEffect Hooks

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
-// const navigation =  
+
 function HomeScreen({ navigation }) {
+  const [type, setType] = useState('todos')
+  const [data, setData] = useState('some data..?')
+
+  useEffect(() => {
+    console.log('type changed');
+    fetch(`https://jsonplaceholder.typicode.com/${type}/1`)
+      .then(response => response.json())
+      // .then(json => console.log(JSON.stringify(json)))
+      .then(json => setData(JSON.stringify(json, null, 4)))
+  }, [type])
+
   return (
     <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Hello Dasha Pikulina</Text>
-      {/* <Button title='to Details' onPress={() => navigation.navigate('a', { text: 'hello from HomeScreen' })} /> */}
+      <Text>Ресурс: {type}</Text>
+      <Button title='Пользователи' onPress={() => setType('users')} />
+      <Button title='Список дел' onPress={() => setType('todos')} />
+      <Button title='Посты' onPress={() => setType('posts')} />
+      <Text>{data}</Text>
+      {/* <Text>{JSON.stringify(data)}</Text> */}
       <Button title='to Details' onPress={() => navigation.navigate('Details', { text: 'hello from HomeScreen' })} />
     </View>
   )
 };
 
 function DetailsScreen({ route, navigation }) {
-  // function DetailsScreen() {
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
@@ -29,7 +39,7 @@ function DetailsScreen({ route, navigation }) {
       <Button title='go Back' onPress={() => navigation.goBack()} />
       <Button title='go Home' onPress={() => navigation.navigate('Home')} />
       <Button title='go Details again...' onPress={() => navigation.push('Details', { text: 'hello from DetailsScreen' })} />
-      <Button title='do Nothing'/>
+      <Button title='do Nothing' />
 
 
     </View>
@@ -37,9 +47,12 @@ function DetailsScreen({ route, navigation }) {
 }
 
 export default function App() {
+  console.log('App rendered')
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='Details' component={DetailsScreen} />
       </Stack.Navigator>
@@ -61,7 +74,6 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   sectionDescription: {
-    // marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
   },
